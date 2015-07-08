1024049942fb8534493e62d2d9bd1d7e945828ee
Dragan Ostojić <dragan.ostojic@bell.net>
2015-07-08 00:26:18 -0400

    Retrieved the query (as one big string)

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.sunshine.app;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -21,6 +22,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -103,28 +105,39 @@ public class MainActivity extends ActionBarActivity {
             // Set Adapter on ListView
             listView.setAdapter(mForecastAdapter);
 
+
+            // Check that there is a network connection before attempting
+            // to issue http GET request (to read data)
+            ConnectivityManager connMgr = (ConnectivityManager)
+                    getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+            if (networkInfo == null || !networkInfo.isConnected()) {
+                Log.e("PlaceholderFragment", "No network connection");
+                return null;
+            }
+            Log.i("PlaceholderFragment", "Has network connection!!!");
+
+            // Allow networking I/O to be done on the main thread
+            // NOTE:
+            // This is a workaround (and for debugging). The standard practice is
+            // to perform networking I/O on a separate thread
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
 
             // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
-
-            ConnectivityManager connMgr = (ConnectivityManager)
-                    getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-            if (networkInfo == null || !networkInfo.isConnected()) {
-                Log.e("PlaceholderFragment", "No network connection");
-                //return rootView;
-                return null;
-            }
-            Log.i("PlaceholderFragment", "Has network connection!!!");
-            /*try {
+            //String forecastJsonStr = null;
+            List<String> forecastJsonStr = new ArrayList<String>();
+            try {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are avaiable at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
                 URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+                //URL url = new URL("http://www.google.com");
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
@@ -133,26 +146,43 @@ public class MainActivity extends ActionBarActivity {
 
                 // Read the input stream into a String
                 InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
+                //StringBuffer buffer = new StringBuffer();
                 if (inputStream == null) {
                     // Nothing to do.
                     return null;
                 }
                 reader = new BufferedReader(new InputStreamReader(inputStream));
 
-                String line;
-                while ((line = reader.readLine()) != null) {
+                //String line;
+                while (true) {
+                    String line = reader.readLine();
+                    if (line == null) {
+                        break;
+                    }
                     // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
                     // But it does make debugging a *lot* easier if you print out the completed
                     // buffer for debugging.
-                    buffer.append(line + "\n");
+                    //buffer.append(line + '\n');
+                    //buffer.append(line);
+                    forecastJsonStr.add(line);
                 }
 
-                if (buffer.length() == 0) {
+                //if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                //    return null;
+                //}
+                if (forecastJsonStr.size() == 0) {
                     // Stream was empty.  No point in parsing.
                     return null;
                 }
-                forecastJsonStr = buffer.toString();
+
+                //forecastJsonStr = buffer.toString();
+
+                Log.d("PlaceholderFragment", "forecastJsonStr.size()=" + forecastJsonStr.size());
+                for (String line : forecastJsonStr) {
+                    Log.d("PlaceholderFragment", line);
+                }
+
             } catch (IOException e) {
                 Log.e("PlaceholderFragment", "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
@@ -169,7 +199,7 @@ public class MainActivity extends ActionBarActivity {
                         Log.e("PlaceholderFragment", "Error closing stream", e);
                     }
                 }
-            }*/
+            }
 
             return rootView;
         }
