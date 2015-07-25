ffc706e0e4431880144fcbe6b1b2ea361a39f543
Dragan Ostojić <dragan.ostojic@bell.net>
2015-07-25 00:07:13 -0400

    Added items to the menu

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.android.sunshine.app;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class
 MainActivity extends ActionBarActivity
@@ -25,8 +27,8 @@ MainActivity extends ActionBarActivity
 	public boolean
 	onCreateOptionsMenu(Menu menu)
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
 		return true;
 	}
 
@@ -34,16 +36,15 @@ MainActivity extends ActionBarActivity
 	public boolean
 	onOptionsItemSelected(MenuItem item)
 	{
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
+		switch (item.getItemId()) {
+			case R.id.action_settings:
+				Toast.makeText(this, R.string.action_settings, Toast.LENGTH_LONG).show();
+				return true;
+			case R.id.help:
+				Toast.makeText(this, R.string.help, Toast.LENGTH_LONG).show();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
 		}
-
-		return super.onOptionsItemSelected(item);
 	}
 }
