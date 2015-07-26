002f4c3b237edb094e7ea79b90c15467a741c23d
Dragan Ostojić <dragan.ostojic@bell.net>
2015-07-26 18:45:11 -0400

    Added extracting min temperature

--- app/src/main/java/com/example/android/sunshine/app/WeatherDataParser.java
+++ app/src/main/java/com/example/android/sunshine/app/WeatherDataParser.java
@@ -6,9 +6,13 @@ import org.json.JSONObject;
 
 public class WeatherDataParser
 {
+	final static String OWM_LIST = "list";
+	final static String OWM_TEMPERATURE = "temp";
+	final static String OWM_MAX = "max";
+	final static String OWM_MIN = "min";
 	/**
 	 * Given a string of the form returned by the api call:
-	 * http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7 
+	 * http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7
 	 * retrieve the maximum temperature for the day indicated by dayIndex
 	 * (Note: 0-indexed, so 0 would refer to the first day).
 	 */
@@ -17,15 +21,23 @@ public class WeatherDataParser
 													int dayIndex)
 													throws JSONException
 	{
-		final String OWM_LIST = "list";
-		final String OWM_TEMPERATURE = "temp";
-		final String OWM_MAX = "max";
-
 		JSONObject weather = new JSONObject(weatherJsonStr);
 		JSONArray days = weather.getJSONArray(OWM_LIST);
 		JSONObject dayInfo = days.getJSONObject(dayIndex);
 		JSONObject temperatureInfo = dayInfo.getJSONObject(OWM_TEMPERATURE);
 		return temperatureInfo.getDouble(OWM_MAX);
+	}
+
+	private static double
+	getMinTemperatureForDay(String weatherJsonStr,
+													int dayIndex)
+		throws JSONException
+	{
+		JSONObject weather = new JSONObject(weatherJsonStr);
+		JSONArray days = weather.getJSONArray(OWM_LIST);
+		JSONObject dayInfo = days.getJSONObject(dayIndex);
+		JSONObject temperatureInfo = dayInfo.getJSONObject(OWM_TEMPERATURE);
+		return temperatureInfo.getDouble(OWM_MIN);
 	}
 
 	public static String[]
@@ -36,10 +48,9 @@ public class WeatherDataParser
 		String[] resultsString = new String[numberOfDays];
 
 		for (int i = 0; i < numberOfDays; i++) {
-			double temperature = 0.0;
 			String day = "Mod, Jun 1";
 			String description = "Clear";
-			long low = Math.round(13.0);
+			long low = Math.round(getMinTemperatureForDay(weatherJsonStr, i));
 			long high = Math.round(getMaxTemperatureForDay(weatherJsonStr, i));
 			resultsString[i] = day + " - " + description + " - " + high + "/" + low;
 		}
